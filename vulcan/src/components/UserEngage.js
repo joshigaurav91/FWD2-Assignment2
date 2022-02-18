9ea884570cda6d9cc1714e2f1d055025a51c51c0
Dana Golubiev <48007635+DanaGolub@users.noreply.github.com>
2022-02-18 14:06:37 -0800

    UserEngage map component centered text, img bcgd added

--- vulcan/src/components/UserEngage.js
+++ vulcan/src/components/UserEngage.js
@@ -1,15 +1,16 @@
+import map from '../images/map.jpg'
+
 function UserEngage() {
     return (
         <div>
             <div className="map">
-                <img src="" alt="" />
-                <div>
+                <img src={map} alt="map of the United States" />
+                <div className="map-content">
                     <h3>INSTALLED IN A CITY NEAR YOU</h3>
                     <a href="#main-nav" class="btn-menu-smscreen" id="btn-menu">FIND A VULCAN INSTALL</a>
                 </div>
             </div>
             <div className="contact">
-            <img src="" alt="" />
                 <div>
                     <h3>READY TO SPEC VULCAN FOR YOUR PROJECT?</h3>
                     <a href="#main-nav" class="btn-menu-smscreen" id="btn-menu">CONTACT US</a>
--- vulcan/src/components/Video.js
+++ vulcan/src/components/Video.js
@@ -1,6 +1,17 @@
 function Video() {
     return (
-        <div>
+        <div className="video-section">
+            <div className="frame">
+                <iframe
+                    width="788"
+                    height="443"
+                    src="https://www.youtube.com/embed/5meKeQn2ZJk"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen>
+                </iframe>
+            </div>
         </div>
     )
 }
